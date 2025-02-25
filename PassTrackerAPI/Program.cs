b313da8cbc6ab73eff67167f83c72d3ce949f5d4
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-02-25 20:13:51 +0700

    configure program.cs

--- PassTrackerAPI/Program.cs
+++ PassTrackerAPI/Program.cs
@@ -1,18 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using PassTrackerAPI.Data;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(connection));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+using var serviceScope = app.Services.CreateScope();
+
+var context = serviceScope.ServiceProvider.GetService<DataContext>();
+
+context?.Database.Migrate();
 
 app.UseHttpsRedirection();
 
